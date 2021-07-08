68a257927f855058af786d1c1882ed42245d060e
tranviethoangsmile <tranviethoangsmile@gmail.com>
2021-07-08 21:25:13 +0700

    update

--- bài tập tại lớp/color.js
+++ bài tập tại lớp/color.js
@@ -1,9 +1,23 @@
-function colorNew() {
-    var red = document.getElementById("red").value;
-    var green = document.getElementById("green").value;
-    var blue = document.getElementById("blue").value;
-    document.getElementById("colorTest").style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
-    document.getElementById("r").innerHTML = red;
-    document.getElementById("g").innerHTML = green;
-    document.getElementById("b").innerHTML = blue;
+function maxMinAvg(arr) {
+    var arrnew = [];
+    var max = arr[0];
+    var min = arr[0];
+    var total = 0;
+    var avg = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (max < arr[i]) {
+            max = arr[i]
+        }
+
+        if (min > arr[i]) {
+            min = arr[i];
+        }
+        total += arr[i];
+        avg = total / arr.length;
+        arrnew.push(max)
+        arrnew.push(min)
+        arrnew.push(avg);
+    }
+    return arrnew;
 }
