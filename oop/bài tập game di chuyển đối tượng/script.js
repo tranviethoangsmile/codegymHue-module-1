034b693996c0dfb460e0572d43963234c4b8cf4e
tranviethoangsmile <tranviethoangsmile@gmail.com>
2021-07-16 10:01:55 +0700

    update

---
+++ oop/bài tập game di chuyển đối tượng/script.js
@@ -0,0 +1,48 @@
+function Hero(image, top, below, left, right, size) {
+  this.image = image;
+  this.top = top;
+  this.left = left;
+  this.size = size;
+
+  this.getHeroElement = function () {
+      return '<img width="' + this.size + '"' +
+          ' height="' + this.size + '"' +
+          ' src="' + this.image + '"' +
+          ' style="top: ' + this.top + 'px; left:' + this.left + 'px; position:absolute;" />';
+  };
+  this.moveRight = function () {
+      this.left += 100;
+  };
+  this.moveBelow = function () {
+      this.top += 100;
+  };
+  this.moveLeft = function () {
+      this.left -= 100;
+  };
+  this.moveTop = function () {
+      this.top -= 100;
+  };
+};
+
+let hero = new Hero("down2.png", 20, 20, 20, 20, 200);
+
+function start() {
+  if (hero.left < window.innerWidth - hero.size && hero.top < 50) {
+      hero.moveRight();
+  } else if (hero.top < window.innerHeight - hero.size && hero.left >= window.innerWidth - 200) {
+      hero.moveBelow();
+  } else if (hero.left > 100) {
+      hero.moveLeft();
+  } else {
+      hero.moveTop();
+  }
+
+  document.getElementById('game').innerHTML = hero.getHeroElement();
+  setTimeout(start, 50)
+}
+
+
+start();
+
+
+
